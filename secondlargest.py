f32c4687c812652c708a84817d22e15924ad516c
GolpalaSandeep <sandeepgolpala@gmail.com>
2025-01-25 15:49:53 +0530

    .

--- secondlargest.py
+++ secondlargest.py
@@ -1,3 +1,15 @@
 def seclargest(arr):
+    max=-float('inf')
+    secmax=-float('inf')
     for i in arr:
-        if
+        if(i>max):
+            secmax=max
+            max=i
+        elif (i>secmax and i!=max):
+            secmax=i
+    return secmax
+arr=[]
+n=int(input("Enter the number of numbers: "))
+for i in range(n):
+    arr.append(int(input()))
+print("second largest is: ",seclargest(arr))
