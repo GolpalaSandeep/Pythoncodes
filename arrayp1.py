3566144aacc24c9679c00f885a5471e3aa1c40fe
GolpalaSandeep <sandeepgolpala@gmail.com>
2025-01-27 11:26:43 +0530

    .

---
+++ arrayp1.py
@@ -0,0 +1,12 @@
+Arr=[]
+for i in range(5):
+    Arr.append(int(input(f"Enter {i} integer: ")))
+summ=0
+for i in range(5):
+    summ=summ+Arr[i]
+print("For loop sum: ",summ)    
+
+
+print("Function sum: ",sum(Arr))
+
+print(Arr)
---
+++ listpython.py
@@ -0,0 +1,35 @@
+L1=[]
+print("1.An empty list is",L1)
+
+L2=[1,2,3,4]
+print("2.The list with four items is",L2)
+
+L3=['abc',["def","ghi"]]
+print("3.The nested list is",L3)
+print(L3[1][1])
+
+L4=list('spam')
+print("5.List is",L4)
+
+L5=list(range(-4,5))
+print("6.List is",L5)
+print("7",L5[3])
+
+
+L8=['a','b','c','d',1,2,3,4,5,6]
+print("8.Slicing: ",L8[2:5])
+
+L9=['a','b','c','d',[1,2,3,4,5,6]]
+print("Exper: ",L9[4][1:4])
+
+print("L1: ",L1)
+print("L2: ",L2)
+print("L3: ",L3)
+print("L4: ",L4)
+print("L5: ",L5)
+print("L8: ",L8)
+print("L9: ",L9)
+print("L9: ",L9)
+
+
+
